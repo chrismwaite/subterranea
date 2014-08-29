4dd0afd46ecfc9fb73fbeed66b20b8dcc2d765ea
Chris <chrismwaite@gmail.com>
2014-08-29 19:28:39 +0100

    Deleting MOAI engine - it can be downloaded separately

--- config/config.lua
+++
@@ -1,7 +0,0 @@
-----------------------------------------------------------------
--- Copyright (c) 2010-2011 Zipline Games, Inc. 
--- All Rights Reserved. 
--- http://getmoai.com
-----------------------------------------------------------------
-
-package.path = package.path .. ';../../moai-sdk/include/lua-modules/moai/?.lua'
